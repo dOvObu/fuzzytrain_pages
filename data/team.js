2dc15d8b20400fb44fa77f2fa27e46e966c7fa82
mOgOm <c0_0b@mail.ru>
2019-03-25 21:54:05 +0300

    team page
    team page

--- data/team.js
+++ data/team.js
@@ -2,8 +2,7 @@ data = [
 	{
 		title : `Без предисловий`,
 		content : [
-			{img : `https://images.unsplash.com/photo-1542463873-e6b71bc145f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80`},
-			{text: `---------------------------`}
+			{img : `https://images.unsplash.com/photo-1542463873-e6b71bc145f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80`}
 		]
 	},
 	{
